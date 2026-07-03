1bc0148290f601cf1a3f3dd83806d80fcbb650e4
nosrednawall <nosrednawall@gmail.com>
2026-07-03 20:19:05 -0300

    update

--- home/copyq.nix
+++ home/copyq.nix
@@ -5,30 +5,4 @@
     copyq
   ];
 
-  systemd.user.services.copyq = {
-    Unit = {
-      Description = "CopyQ Clipboard Manager";
-      After = [ "graphical-session.target" ];
-      Wants = [ "graphical-session.target" ];
-      PartOf = [ "graphical-session.target" ];
-      ConditionEnvironment = "DISPLAY";
-    };
-
-    Service = {
-      Type = "simple";
-      # Adiciona a flag --start-server para iniciar o servidor
-      ExecStart = "${pkgs.copyq}/bin/copyq --start-server --config-dir %h/.config/copyq";
-      Restart = "on-failure";
-      RestartSec = 5;
-      TimeoutStopSec = 10;
-      Environment = [
-        "DISPLAY=:0"
-        "QT_QPA_PLATFORM=xcb"
-      ];
-    };
-
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
-  };
 }
--- scripts/dwm/autostart.sh
+++ scripts/dwm/autostart.sh
@@ -12,6 +12,8 @@ is_running "xautolock" || xautolock -time 15 -locker ~/.local/bin/dwm/dwm-slock 
 # Inicia o picom em modo background
 is_running "picom" || picom -b
 
+copyq --start-server --config-dir ~/.config/copyq &
+
 # Inicia o dunst se não estiver rodando
 #is_running "dunst" || dunst -conf "$HOME/.config/dunst/themes/${THEME_MODE}_${COLOR_MODE}" &
 #is_running "dunst" || dunst -conf "$HOME/.config/dunst/dunstrc" &
