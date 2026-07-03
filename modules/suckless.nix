7ac68581bccccc6dc7afaefed5e9950bc1c83154
nosrednawall <nosrednawall@gmail.com>
2026-07-03 09:22:20 -0300

    update

--- modules/suckless.nix
+++ modules/suckless.nix
@@ -52,7 +52,7 @@ let
         --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath old.buildInputs}
       # Também wrap do stest (usado pelo dmenu_run)
       wrapProgram $out/bin/stest \
-        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath buildInputs}
+        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath old.buildInputs}
     '';
   });
 in
