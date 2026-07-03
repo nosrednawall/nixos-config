c81b8ecc00658b4e152880f0a7d308b1e423c0ec
nosrednawall <nosrednawall@gmail.com>
2026-07-03 11:39:00 -0300

    update

--- modules/suckless.nix
+++ modules/suckless.nix
@@ -5,11 +5,11 @@ let
   baseLibs = with pkgs; [
      makeWrapper  # ← ADICIONE ISTO!
      #xorg.libX11
-     #xorg.libXinerama
-     #xorg.libXft
-     #xorg.libXrender
-     #xorg.libXres
-     #xorg.libXrandr
+     xorg.libXinerama
+     xorg.libXft
+     xorg.libXrender
+     xorg.libXres
+     xorg.libXrandr
      libxcb
      libxcb-wm
      libxcb-util
