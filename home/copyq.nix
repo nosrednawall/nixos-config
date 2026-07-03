2030b9b41f87ee840302ede8b96b794a090a08be
nosrednawall <nosrednawall@gmail.com>
2026-07-03 20:22:25 -0300

    update

--- home/copyq.nix
+++ home/copyq.nix
@@ -5,4 +5,26 @@
     copyq
   ];
 
+  # Cria uma configuração limpa
+  home.file.".config/copyq/copyq.conf".text = ''
+    [General]
+    autostart=false
+    showTray=true
+    saveOnExit=true
+    maxClipboardItems=50
+    shortcuts=ctrl+alt+v
+    theme=default
+    language=pt_BR
+    style=Default
+  '';
+
+  # Opcional: remove scripts antigos
+  home.activation = {
+    cleanupCopyq = ''
+      if [ -d "$HOME/.config/copyq" ]; then
+        # Remove scripts problemáticos
+        rm -f "$HOME/.config/copyq/scripts"/* 2>/dev/null || true
+      fi
+    '';
+  };
 }
