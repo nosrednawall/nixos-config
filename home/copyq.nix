3b7a07973691c478c538eb6b3ca68bf34917afa3
nosrednawall <nosrednawall@gmail.com>
2026-07-04 10:52:34 -0300

    update

--- home/copyq.nix
+++ home/copyq.nix
@@ -5,17 +5,4 @@
     copyq
   ];
 
-
-
-
-  home.file.".xprofile".text = ''
-    # Garante o PATH do NixOS para o .xprofile
-    export PATH="$HOME/.nix-profile/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$PATH"
-
-    # Aguarda o servidor X iniciar
-    sleep 2
-
-    # Inicia o CopyQ usando o caminho do perfil do usuário
-    ${pkgs.copyq}/bin/copyq --start-server --disable-tray &
-  '';
 }
