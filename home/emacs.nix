d5a2ac6f048696eb9e6a8258f59322b4768c15c4
nosrednawall <nosrednawall@gmail.com>
2026-07-04 19:29:02 -0300

    update

--- home/emacs.nix
+++ home/emacs.nix
@@ -14,17 +14,13 @@
     recursive = true;
   };
 
-  # Script para instalar o Doom
+  # Copia sua configuração local do Doom
   home.file.".local/bin/install-doom" = {
-    text = ''
-      #!/usr/bin/env bash
-      if [ ! -d "$HOME/.config/emacs" ]; then
-        git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-        ~/.config/emacs/bin/doom install
-      fi
-    '';
-    executable = true;
+    source = ../scripts/install-doom;  # Pasta local com init.el, config.el, packages.el
+    recursive = true;
   };
+
+
 
   home.activation.installDoom = lib.hm.dag.entryAfter ["writeBoundary"] ''
     if [ ! -d "$HOME/.config/emacs" ]; then
